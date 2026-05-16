ac7836fe29523550ec552ab51b04859163d51ded
Kumar Rohit <91978766+kumarrohit2509@users.noreply.github.com>
2026-05-16 17:28:01 +0530

    adding services/arxiv services

---
+++ src/schemas/arxiv/paper.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from typing import Any, Dict, List, Optional
+from uuid import UUID
+
+from pydantic import BaseModel, Field
+
+class ArxivPaper(BaseModel):
+     '''
+     Schema for the Arxiv API response
+     '''
+     arxiv_id: str = Field(..., description="Arxiv Paper Id")
+     title: str = Field(..., description="Paper title")
+     authors: List[str] = Field(...,description="Paper abstract")
+     published_date: str = Field(..., description="Date published on arXiv (ISO format)")
+     categories: List[str] = Field(...,description="Paper categories")
+     pdf_url: str = Field(..., description="URL to PDF")
+
+class PaperBase(BaseModel):
+     pass
+     

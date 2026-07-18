230dce1f432b4193c4fdd5c98009b038e9003d2c
Kumar Rohit <93335681+k-Rohit@users.noreply.github.com>
2026-07-18 20:37:39 +0530

    refactor: remove unused imports from docling_parser.py

--- src/services/pdf_parser/docling_parser.py
+++ src/services/pdf_parser/docling_parser.py
@@ -22,7 +22,7 @@ from docling.datamodel.base_models import InputFormat
 from docling.datamodel.pipeline_options import PdfPipelineOptions
 from docling.document_converter import DocumentConverter, PdfFormatOption
 from src.exceptions import PDFParsingException, PDFValidationError
-from src.schemas.pdf_parser.models import PaperFigure, PaperSection, PaperTable, ParserType, PdfContent
+from src.schemas.pdf_parser.models import PaperSection, ParserType, PdfContent
 
 logger = logging.getLogger(__name__)
 
